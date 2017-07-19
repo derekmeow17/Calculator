045042d6a4763d0d328723dfeeac683a7d1a4896
derekmeow17 <derek.lyc1027@gmail.com>
2017-07-19 23:36:03 +0800

    Add new test cases

--- CalculatorTest.java
+++ CalculatorTest.java
@@ -6,15 +6,16 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
-	
+
 	private Calculator calculator;
-		
+
 	@Before
 	public void setUp() {
 		calculator = new Calculator();
 	}
+
 	@Test
-	public void when_calculating_empty_string_should_return_0() {
+	public void formula_empty_string_returns_0() {
 		// Method you are testing
 		Double result = calculator.calculate("");
 		// Result you expect
@@ -22,39 +23,63 @@ public class CalculatorTest {
 	}
 
 	@Test
-	public void when_calculating_string_one_should_return_1() {
+	public void formula_string_1_returns_1() {
 		Double result = calculator.calculate("1");
 		assertEquals(new Double(1), result);
 	}
-	
+
 	@Test
-	public void when_calculating_2_minus_1_should_return_1() {
+	public void formula_2_minus_1_returns_1() {
 		Double result = calculator.calculate("2-1");
 		assertEquals(new Double(1), result);
 	}
-	
+
 	@Test
-	public void when_calculating_1_plus_1_plus_2_should_return_4() {
+	public void formula_1_plus_1_plus_2_returns_4() {
 		Double result = calculator.calculate("1+1+2");
 		assertEquals(new Double(4), result);
 	}
-	
+
 	@Test
-	public void when_calculating_1_plus_1_plus_2_minus_4_should_return_0() {
+	public void formula_1_plus_1_plus_2_minus_4_returns_0() {
 		Double result = calculator.calculate("1+1+2-4");
 		assertEquals(new Double(0), result);
 	}
-	
+
 	@Test
-	public void when_calculating_1_multiplies_2_plus_2_multiplies_n8_should_return_n14() {
+	public void formula_1_plus_1_plus_2_times_4_minus_5_returns_5() {
+		Double result = calculator.calculate("1+1+2*4-5");
+		assertEquals(new Double(5), result);
+	}
+
+	@Test
+	public void formula_1_times_2_plus_2_times_n8_returns_n14() {
 		Double result = calculator.calculate("1*2+2*-8");
 		assertEquals(new Double(-14), result);
 	}
-	
+
 	@Test
-	public void when_calculating_1_multiplies_2_plus_2_multiplies_n8_should_return_n() {
+	public void formula_complex_ODMAS_1_returns_11() {
 		Double result = calculator.calculate("3-1*16/8+6+2^2");
 		assertEquals(new Double(11), result);
+	}
+
+	@Test
+	public void formula_complex_ODMAS_2_returns_8() {
+		Double result = calculator.calculate("4^3+16/-4*9-20");
+		assertEquals(new Double(8), result);
+	}
+
+	@Test
+	public void formula_complex_BODMAS_1_returns_11() {
+		Double result = calculator.calculate("1+(2+3)*4-5*(4/2)");
+		assertEquals(new Double(11), result);
+	}
+
+	@Test
+	public void formula_complex_BODMAS_2_returns_n4() {
+		Double result = calculator.calculate("2^(2*4-5)/(-9+5)*2");
+		assertEquals(new Double(-4), result);
 	}
 
 }
