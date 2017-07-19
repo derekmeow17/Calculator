7e9c71ce171f522d75ea84d71b7fde84d868f1ec
derekmeow17 <derek.lyc1027@gmail.com>
2017-07-19 23:35:24 +0800

    Tidy up codes

--- Calculator.java
+++ Calculator.java
@@ -2,35 +2,24 @@ package git.Calculator;
 
 public class Calculator {
 
-	public static void main(String[] args) {
-		System.out.println(parseExp(""));
-	}
-
-	public static int findLastMinus(String s, int lastIndex) {
-		int index = s.lastIndexOf('-', lastIndex);
+	int findValidOp(String s, char op, int lastIndex) {
+		int index = s.lastIndexOf(op, lastIndex);
 		if (index <= 0) // -1: can't find -ve sign; 0: 1st num is -ve
 			return -1;
 		else if (s.charAt(index - 1) == '+' || s.charAt(index - 1) == '-' || s.charAt(index - 1) == '*'
 				|| s.charAt(index - 1) == '/' || s.charAt(index - 1) == '^')
-			return findLastMinus(s, index - 1);
+			return findValidOp(s, op, index - 1);
 		else
 			return index;
 	}
 
-	public static String parseTerm(String evaluate) {
-
-		int opIndex = evaluate.length() - 1;
-		if (evaluate.contains("+")) {
-			if (opIndex == evaluate.length() - 1 || evaluate.lastIndexOf('+') > opIndex)
-				opIndex = evaluate.lastIndexOf('+');
-		}
-		if (evaluate.contains("-")) {
-			int tmp = findLastMinus(evaluate, evaluate.length() - 1);
-			if ((opIndex == evaluate.length() - 1 || tmp > opIndex) && tmp != -1) {
-				opIndex = tmp;
-			}
-		}
-		if (opIndex == evaluate.length() - 1) {
+	String parseTerm(String evaluate) {
+		// Find last add/sub operator in the formula
+		int opIndex = findValidOp(evaluate, '+', evaluate.length() - 1);
+		int tmp = findValidOp(evaluate, '-', evaluate.length() - 1);
+		if (tmp > opIndex)
+			opIndex = tmp;
+		if (opIndex == -1) {
 			return parseFactor(evaluate);
 		} else {
 			String left = evaluate.substring(0, opIndex);
@@ -39,18 +28,13 @@ public class Calculator {
 		}
 	}
 
-	public static String parseFactor(String evaluate) {
-		int opIndex = evaluate.length() - 1;
-		if (evaluate.contains("*")) {
-			if (opIndex == evaluate.length() - 1 || evaluate.lastIndexOf('*') > opIndex)
-				opIndex = evaluate.lastIndexOf('*');
-		}
-		if (evaluate.contains("/")) {
-			if (opIndex == evaluate.length() - 1 || evaluate.lastIndexOf('/') > opIndex) {
-				opIndex = evaluate.lastIndexOf('/');
-			}
-		}
-		if (opIndex == evaluate.length() - 1) {
+	String parseFactor(String evaluate) {
+		// Find last mul/div operator in the formula
+		int opIndex = evaluate.lastIndexOf('*');
+		int tmp = evaluate.lastIndexOf('/');
+		if (tmp > opIndex)
+			opIndex = tmp;
+		if (opIndex == - 1) {
 			return parsePow(evaluate);
 		} else {
 			String left = evaluate.substring(0, opIndex);
@@ -59,7 +43,8 @@ public class Calculator {
 		}
 	}
 
-	public static String parsePow(String evaluate) {
+	String parsePow(String evaluate) {
+		// Find first pow operator in the formula
 		int opIndex = evaluate.indexOf('^');
 		if (opIndex == -1)
 			return evaluate;
@@ -70,13 +55,13 @@ public class Calculator {
 		}
 	}
 
-	public static String parseExp(String evaluate) {
+	String parseExp(String evaluate) {
 		int openBracketIndex = evaluate.lastIndexOf('(');
 		int closeBracketIndex = evaluate.indexOf(')', openBracketIndex);
 		if (openBracketIndex == -1 && closeBracketIndex == -1)
 			return parseTerm(evaluate);
 		else {
-			String tmp; // To store current bracket and contents inside
+			String tmp; // To store current bracket and the contents inside
 			if (closeBracketIndex == evaluate.length() - 1)
 				tmp = evaluate.substring(openBracketIndex);
 			else
@@ -85,47 +70,39 @@ public class Calculator {
 		}
 	}
 
-	public Double calculate(String evaluate) {
+	Double calculate(String evaluate) {
 		// TODO Auto-generated method stub
 		if (evaluate.isEmpty()) {
 			return new Double(0);
 		}
-		return Double.parseDouble(parseTerm(evaluate));
+		return Double.parseDouble(parseExp(evaluate));
 	}
 
-	public static String opAddSub(String num1, String num2, char op) {
+	String opAddSub(String num1, String num2, char op) {
 		Double result = new Double(0);
-		switch (op) {
-		case '+':
+		if (op == '+')
 			result = Double.parseDouble(num1) + Double.parseDouble(num2);
-			break;
-		case '-':
+		else
 			result = Double.parseDouble(num1) - Double.parseDouble(num2);
-			break;
-		}
 		return result.toString();
 	}
 
-	public static String opMulDiv(String num1, String num2, char op) {
+	String opMulDiv(String num1, String num2, char op) {
 		Double result = new Double(0);
-		switch (op) {
-		case '*':
+		if (op == '*')
 			result = Double.parseDouble(num1) * Double.parseDouble(num2);
-			break;
-		case '/':
+		else
 			result = Double.parseDouble(num1) / Double.parseDouble(num2);
-			break;
-		}
 		return result.toString();
 	}
 
-	public static String opPow(String s1, String s2) {
+	String opPow(String s1, String s2) {
 		double num1 = Double.parseDouble(s1);
 		double num2 = Double.parseDouble(s2);
 		return calculatePow(num1, (int) num2).toString();
 	}
 
-	public static Double calculatePow(double num1, double num2) {
+	Double calculatePow(double num1, double num2) {
 		if (num1 == 0)
 			return new Double(0);
 		else if (num2 == 0)
